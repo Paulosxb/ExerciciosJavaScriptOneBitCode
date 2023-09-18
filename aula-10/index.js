71bed441cfa324b0050ff71ea3547d339f9b8a60
Paulo Sergio <paulosxb@gmail.com>
2023-09-18 11:03:43 -0300

    pasta de exercicios
    exercícios gerais

--- aula-10/index.js
+++ aula-10/index.js
@@ -1,4 +1,13 @@
 let nome = "Paulo"
 
+//Recursos nativos 
 console.log(nome)
+console.error(nome)
+console.warn(nome)
 
+//window.alert(nome)
+//nome = window.prompt("Insira seu nome:")
+
+let confirmacao = window.confirm("Confirma essa ação?")
+
+console.log(nome)
