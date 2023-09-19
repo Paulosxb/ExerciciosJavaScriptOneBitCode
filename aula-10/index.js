12d95ae9e0800541c8539a645cd3390dc2e63825
Paulo Sergio <paulosxb@gmail.com>
2023-09-19 16:42:27 -0300

    atualizar
    comentarios incluidos

--- aula-10/index.js
+++ aula-10/index.js
@@ -1,13 +1,20 @@
+//Você está declarando uma variável chamada nome e atribuindo o valor "Paulo" a ela.
 let nome = "Paulo"
 
-//Recursos nativos 
+//Este comando imprime o valor da variável nome no console do navegador. É uma forma comum de depuração.
 console.log(nome)
-console.error(nome)
+
+//Este comando imprime o valor da variável nome como um erro no console. Isso é útil para destacar problemas durante a depuração.
+console.error(nome) 
+
+//Este comando imprime o valor da variável nome como um aviso no console. É útil para destacar algo que pode ser um problema, mas não é 
 console.warn(nome)
 
-//window.alert(nome)
-//nome = window.prompt("Insira seu nome:")
+//Este comando exibe uma caixa de diálogo de alerta com o valor da variável nome.
+window.alert(nome)
 
+//Este comando exibe uma caixa de diálogo que pede ao usuário para inserir seu nome. O valor inserido substituirá o valor atual da variável nome.
+nome = window.prompt("Insira seu nome:")
+
+Este comando exibe uma caixa de diálogo de confirmação com os botões "OK" e "Cancelar". O resultado (verdadeiro ou falso) é armazenado na variável confirmacao.
 let confirmacao = window.confirm("Confirma essa ação?")
-
-console.log(nome)
