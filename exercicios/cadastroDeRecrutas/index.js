06dd39c2cb929dcd79dcf842f9342e5d070620e4
Paulo Sergio <paulosxb@gmail.com>
2023-09-19 17:06:12 -0300

    resolução dos exercicios
    Foi criado os exercicios de Cadastro de recruta e Calculadora de 4 operadores

---
+++ exercicios/cadastroDeRecrutas/index.js
@@ -0,0 +1,13 @@
+const primeiroNome = prompt ("Informe o primeiro nome do recruta:")
+const sobrenome = prompt ("Informe o sobrenome do recruta")
+const campoDeEstudo = prompt ("Qual é o campo de estudo do recruta?")
+const anoDeNascimento = prompt ("Qual é o ano de nascimento do recruta?")
+
+alert (
+    "Recruta cadastrado com sucesso!\n" + 
+    "\nNome Completo: " + primeiroNome + " " + sobrenome + 
+    "\nCampo de Estudo: "  + campoDeEstudo +
+    "\nAno Nascimento: "   + (2023 - anoDeNascimento)
+)
+
+
